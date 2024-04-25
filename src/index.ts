fdcdc0fa4fac0da85564a4a81d73f068084340c5
Michael Epelboim <michael.epelboim@tromero.ai>
2024-04-25 19:55:00 +0200

    small changes

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,19 @@
-import express, { Express } from 'express';
 import dotenv from 'dotenv';
+dotenv.config();
+import express, { Express } from 'express';
 import router from './router';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
-dotenv.config();
-
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use('/', router);
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at port: ${port}`);
 });
